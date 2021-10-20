7ce87d6cee16717fcd323f49f2e4ac6b0482ac9e
Serhiy Dvoryannikov <dvoryannikovserhiy@gmail.com>
2021-10-20 21:14:33 +0300

    promisify-function done

---
+++ promisify-function/app.js
@@ -0,0 +1,38 @@
+async function fetchUser(id, callback) {
+	const users = {
+		1: {
+			uid: 1,
+			name: "John Doe",
+			age: 22,
+		},
+	};
+
+	const selectedUser = users[id];
+
+	const error = selectedUser ? null : "No User Found";
+
+	return new Promise((resolve) => {
+		setTimeout(function () {
+			resolve(callback(error, selectedUser));
+		}, 2000);
+	});
+}
+
+const promisify = (fn) => {
+	return async function (id) {
+		return await fn(id, function (error, selectedUser) {
+			return new Promise((resolve, reject) => {
+				if (error) {
+					reject(error);
+				} else {
+					resolve(selectedUser);
+				}
+			});
+		});
+	};
+};
+
+const promisifiedFetchUser = promisify(fetchUser);
+promisifiedFetchUser(1)
+	.then((response) => console.log(`User data: ${JSON.stringify(response)}`))
+	.catch((error) => console.log(`An error happened: ${error}`));
