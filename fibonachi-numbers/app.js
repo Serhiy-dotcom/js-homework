36e91f6977a0be0f039412701bd79a9eee253ae5
Serhiy Dvoryannikov <dvoryannikovserhiy@gmail.com>
2021-10-20 21:14:07 +0300

    fibonachi-numbers task

---
+++ fibonachi-numbers/app.js
@@ -0,0 +1,19 @@
+const generator = (num) => {
+	let f1 = 1,
+		f2 = 1;
+	before = 1;
+
+	if (num == 1 || num == 2) {
+		return 1;
+	}
+
+	for (let i = 3; i <= num; i++) {
+		f2 = before;
+		before = f1;
+		f1 = f1 + f2;
+	}
+
+	return f1;
+};
+
+console.log(generator(3));
