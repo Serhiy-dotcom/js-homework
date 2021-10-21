73202d860bf0c77df7b6a9e2992bfbd5b13d82c1
Serhiy Dvoryannikov <dvoryannikovserhiy@gmail.com>
2021-10-21 15:47:19 +0300

    fix fibonacci numbers code, from simple number to array

--- fibonachi-numbers/app.js
+++ fibonachi-numbers/app.js
@@ -1,19 +1,28 @@
 const generator = (num) => {
-	let f1 = 1,
+	let fibList = [],
+		f1 = 1,
 		f2 = 1,
 		before = 1;
 
 	if (num == 1 || num == 2) {
-		return 1;
+		for (let i = 0; i < 2; i++) {
+			fibList.push(1);
+		}
+		return fibList;
+	} else {
+		for (let i = 0; i < 2; i++) {
+			fibList.push(1);
+		}
 	}
 
 	for (let i = 3; i <= num; i++) {
 		f2 = before;
 		before = f1;
 		f1 = f1 + f2;
+		fibList.push(f1);
 	}
 
-	return f1;
+	return fibList;
 };
 
-console.log(generator(3));
+console.log(generator(6));
