ad61e75677bf36578cd363e3f9d51a861453b2f0
Serhiy Dvoryannikov <dvoryannikovserhiy@gmail.com>
2021-11-01 19:08:24 +0200

    did fibonachhi optimizution

--- fibonachi-numbers/app.js
+++ fibonachi-numbers/app.js
@@ -1,27 +1,11 @@
 const generator = (num) => {
-	let fibList = [],
-		f1 = 1,
-		f2 = 1,
-		before = 1;
-
-	if (num == 1 || num == 2) {
-		for (let i = 0; i < 2; i++) {
-			fibList.push(1);
-		}
-		return fibList;
-	} else {
-		for (let i = 0; i < 2; i++) {
-			fibList.push(1);
-		}
-	}
-
-	for (let i = 3; i <= num; i++) {
-		f2 = before;
-		before = f1;
-		f1 = f1 + f2;
-		fibList.push(f1);
-	}
-
+	let fibList = [];
+	for (
+		let i = 1, f2 = 1, f1 = 1, before = 1;
+		i <= num;
+		i++, f2 = before, before = f1, f1 += f2
+	)
+		i < 3 ? fibList.push(1) : fibList.push(f1);
 	return fibList;
 };
 
