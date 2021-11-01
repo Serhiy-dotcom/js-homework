c56c370184811032570fe3c09ddd55acabb6043a
Serhiy Dvoryannikov <dvoryannikovserhiy@gmail.com>
2021-11-01 20:16:11 +0200

    totally solved this task)

--- fibonachi-numbers/app.js
+++ fibonachi-numbers/app.js
@@ -1,12 +1,14 @@
-const generator = (num) => {
-	let fibList = [];
-	for (
-		let i = 1, f2 = 1, f1 = 1, before = 1;
-		i <= num;
-		i++, f2 = before, before = f1, f1 += f2
-	)
-		i < 3 ? fibList.push(1) : fibList.push(f1);
+const generator = (num, fibList = [], f2 = 1, before = 1, f1 = 1, i = 1) => {
+	if (i < 2) return generator(num, [...fibList, 1], f2, before, f1, ++i);
+	else if (i <= num)
+		return generator(
+			num,
+			[...fibList, f1],
+			(f2 = before),
+			(before = f1),
+			(f1 += f2),
+			++i
+		);
 	return fibList;
 };
-
-console.log(generator(6));
+console.log(generator(23));
