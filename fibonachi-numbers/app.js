36d73ad30440c10319c1053cfe09c160a757a02b
Serhiy Dvoryannikov <dvoryannikovserhiy@gmail.com>
2021-11-02 10:40:55 +0200

    totaly fixed: transform array list to number

--- fibonachi-numbers/app.js
+++ fibonachi-numbers/app.js
@@ -1,14 +1,14 @@
-const generator = (num, fibList = [], f2 = 1, before = 1, f1 = 1, i = 1) => {
-	if (i < 2) return generator(num, [...fibList, 1], f2, before, f1, ++i);
+const generator = (num, result = 0, f2 = 1, before = 1, f1 = 1, i = 1) => {
+	if (i < 2) return generator(num, 1, f2, before, f1, ++i);
 	else if (i <= num)
 		return generator(
 			num,
-			[...fibList, f1],
+			f1,
 			(f2 = before),
 			(before = f1),
 			(f1 += f2),
 			++i
 		);
-	return fibList;
+	return result;
 };
-console.log(generator(23));
+console.log(generator(10));
