03ec8d03c0933da9e7bfa81458208db2ab2e3ded
Serhiy Dvoryannikov <dvoryannikovserhiy@gmail.com>
2021-11-02 11:32:28 +0200

    seems done

--- fibonachi-numbers/app.js
+++ fibonachi-numbers/app.js
@@ -1,14 +1,8 @@
-const generator = (num, result = 0, f2 = 1, before = 1, f1 = 1, i = 1) => {
-	if (i < 2) return generator(num, 1, f2, before, f1, ++i);
-	else if (i <= num)
-		return generator(
-			num,
-			f1,
-			(f2 = before),
-			(before = f1),
-			(f1 += f2),
-			++i
-		);
-	return result;
+const generator = (num, result = 0, f1 = 1, i = 1) => {
+	return i < 3
+		? generator(num, 1, f1, ++i)
+		: i <= num
+		? generator(num, (result += f1), result - f1, ++i)
+		: result;
 };
-console.log(generator(10));
+console.log(generator(17));
